f484b9d038f5ec7e3f2425be34f02a08230a898e
saravanan vallimanalan <saravanan.vallimanalan@outlook.com>
2026-03-27 23:50:46 +0530

    feat: Add CNAME for custom domain and adjust Vite base path and server allowed hosts.

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/',
   plugins: [react(), tailwindcss()],
-  server: {
-    allowedHosts: ['s-p-a-r-r-o-w.github.io'],
-  },
 })
